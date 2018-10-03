1b8085c9a0773ab8e7b3535ad6b3cfab5c9926bb
Charunirathnayake <charunirathnayake1995@gmail.com>
2018-10-03 23:58:59 +0800

    Square numbers
    It gives squre numbers.

---
+++ src/squarenum/SquareNum.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package squarenum;
+
+    import java.util.Scanner;
+
+public class SquareNum {
+    
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        System.out.println(n+"Square number is"+n*n);
+        
+           }
+    
+}
